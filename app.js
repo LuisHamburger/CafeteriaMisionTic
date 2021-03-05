eb788658bb37102c8ccb0b10420cf803d433e4d9
LuisHamburger <luishamburger1152@hotmail.com>
2021-03-04 20:26:36 -0500

    VersionF1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var session = require("express-session");
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const passport = require('passport');
+
 
 //Llamar las rutas aquí
 
@@ -30,8 +30,7 @@ app.use(session({
   saveUninitialized: true,
   cookie: { maxAge: 60000 }
 }));
-app.use(passport.initialize());
-app.use(passport.session());
+
 
 //Establecer las rutas aquí
 app.use('/', paginaInicioRouter);
@@ -60,6 +59,6 @@ module.exports = app;
 
 
 //-----------------------------------------------------------------------------------------------------------------------
-app.listen(3000,() => {
-  console.log(`Example app listening at http://localhost:3000`)
+app.listen(process.env.PORT || 5000,() => {
+  console.log("Example app listening at Puerto")
 });
